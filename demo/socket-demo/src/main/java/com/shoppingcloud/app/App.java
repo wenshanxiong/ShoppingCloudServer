1fa1ee54bde3524494b010f674a4db06da891059
mshamsi2 <mshamsi2@illinois.edu>
2020-08-29 14:21:04 -0500

    Configured for aws server

--- demo/socket-demo/src/main/java/com/shoppingcloud/app/App.java
+++ demo/socket-demo/src/main/java/com/shoppingcloud/app/App.java
@@ -1,8 +1,10 @@
 package com.shoppingcloud.app;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 /**
@@ -17,18 +19,19 @@ public class App {
      * But websocket might be the go to when developing real websocket. Benefits being
      * ability to broad cast to all clients (e.g. one person making an update) and scalability (horizontal).
      */
-    public static void startServer() {
+    public static void startServer() throws UnknownHostException {
         // Declarations
 
         // Constants
         final int port = 6969;
+        final InetAddress serverIP = InetAddress.getByName("18.219.246.53");
         final String charSet = "UTF-8";
         // Variables
         boolean clientTerminated = false;
 
         // Try listening to specified port on local host
         // Multithread capability can be added by extending "Thread"
-        try (ServerSocket server = new ServerSocket(port)) {
+        try (ServerSocket server = new ServerSocket(port, 50, serverIP)) {
             // If valid client, connect
             Socket socketCnx = server.accept();
 
@@ -61,6 +64,10 @@ public class App {
      */
     public static void main(String[] args) {
         System.out.println( "Test Socket" );
-        startServer();
+        try {
+            startServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
